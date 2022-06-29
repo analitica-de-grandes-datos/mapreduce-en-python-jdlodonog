69d86b7da00bf3566481a799e5ed5e16ff48025a
jdlodonog <101781006+jdlodonog@users.noreply.github.com>
2022-06-28 23:32:21 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+
+        key, value = line.split('\t') 
+        value =list(value.strip().split(',')) 
+        key = key.zfill(4) 
+        
+
+        for letra in valor: 
+            sys.stdout.write("{}\t{}\n".format(letra ,key))
