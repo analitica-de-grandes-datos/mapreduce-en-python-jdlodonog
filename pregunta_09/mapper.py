cf328bd1925577f445e1ac0ed9d5046bd5697c22
jdlodonog <101781006+jdlodonog@users.noreply.github.com>
2022-06-28 23:22:53 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+from operator import index
+import sys
+if __name__ == "__main__":
+
+
+    for line in sys.stdin:
+
+
+        columna3 = line.split('   ')[2].strip() 
+        columna3 = columna3.zfill(4) 
+
+        sys.stdout.write("{}   {}   {}\n".format(columna3, str(line.split('   ')[0]), line.split('   ')[1]))
