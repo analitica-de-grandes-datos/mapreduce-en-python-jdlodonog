493254b387ca0773a89f4433e1739e28bb9135cf
jdlodonog <101781006+jdlodonog@users.noreply.github.com>
2022-06-28 23:24:11 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+#
+# reduce  elementos con la misma clave
+#
+if __name__ == "__main__":
+    contador = 0
+
+    for line in sys.stdin:
+
+        lista9 = line.split('   ')
+
+        sys.stdout.write("{}   {}   {}\n".format(lista9[1], lista9[2].strip(), int(lista9[0]))) 
+        if contador >= 5: 
+            break 
+        contador += 1
