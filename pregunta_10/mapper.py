5085afc873526cdd1cdc8c78ea4a351e2743c503
jdlodonog <101781006+jdlodonog@users.noreply.github.com>
2022-06-28 23:37:49 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -4,17 +4,15 @@
 import sys
 if __name__ == "__main__":
 
-    #
-    # itera sobre cada linea de codigo recibida
-    # a traves del flujo de entrada
-    #
+   
     for line in sys.stdin:
 
-
-        key, value = line.split('\t') 
-        value =list(value.strip().split(',')) 
-        key = key.zfill(4) 
+      
+        clave, valor = line.split('\t') 
+        valor =list(valor.strip().split(',')) 
+        clave = clave.zfill(4) 
         
-
+            
+           
         for letra in valor: 
-            sys.stdout.write("{}\t{}\n".format(letra ,key))
+            sys.stdout.write("{}\t{}\n".format(letra ,clave))
