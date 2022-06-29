d022c2154914cfd80a068b4c5e988b0e20f811a7
jdlodonog <101781006+jdlodonog@users.noreply.github.com>
2022-06-28 21:52:13 -0500

    Update reducer.py

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,32 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    v_aux  = 0
+
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = int(val)
+
+        if key == curkey:
+
+            if val > v_aux:
+                v_aux = val
+        else:
+
+            if curkey is not None:
+
+                sys.stdout.write("{}\t{}\n".format(curkey, v_aux))
+
+           # curkey = key
+           # v_aux = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, v_aux))
