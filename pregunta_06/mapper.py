3549a6a2d9b7839c15efb850e72bf87664d86fb5
jdlodonog <101781006+jdlodonog@users.noreply.github.com>
2022-06-28 22:51:20 -0500

    Update mapper.py

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -5,8 +5,19 @@
 import sys
 if __name__ == "__main__":
 
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
     for line in sys.stdin:
 
-        lista6 = line.split(" ")
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+        lista = line.split("   ")
             
-        sys.stdout.write("{}\t{}\n".format(lista6[0],float(lista6[2])))
+            #
+            # escribe al flujo estandar de salida
+            #
+        sys.stdout.write("{}\t{}\n".format(lista[0],float(lista[2])))
