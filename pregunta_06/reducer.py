c48ef0b5ef4f86453f7d5ce67fcb3eb8a21f56fd
jdlodonog <101781006+jdlodonog@users.noreply.github.com>
2022-06-28 22:51:58 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -11,22 +11,34 @@ if __name__ == '__main__':
     curkey = None
     v_max  = 0
 
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
     for line in sys.stdin:
 
         key, val = line.split("\t")
         val = float(val)
 
         if key == curkey:
-    
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
             if val > v_max:
                 v_max = val
             if val < v_min:
                 v_min = val
 
         else:
-   
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
             if curkey is not None:
-      
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
                 sys.stdout.write("{}\t{}\t{}\n".format(curkey, v_max, v_min))
 
             curkey = key
