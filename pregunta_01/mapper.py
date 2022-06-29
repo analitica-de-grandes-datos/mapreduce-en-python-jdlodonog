3fd12466132e8551627b0692694d68b6b3932e67
jdlodonog <101781006+jdlodonog@users.noreply.github.com>
2022-06-28 21:39:06 -0500

    Pr1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+# Se define una funcion que mapea la entrada de las parejas clave valor 
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # Luego  se itera sobre cada linea de codigo recibida a traves del flujo de entrada
+    for line in sys.stdin:
+        # genera las tuplas palabra \tabulador 1 en el conteo de palabras
+        lista = line.split(",")
+            
+            
+        # se escribe el en salida 
+            
+        sys.stdout.write("{}\t1\n".format(lista[2]))
